c218381345159ccdf12e37c98bc209a70df9fe9b
tanzen8labs <tan.nguyen@zen8labs.com>
2025-06-28 16:46:53 +0700

    Comment out GitHub Actions configuration in next.config.js and next.config.ts for clarity

--- next.config.ts
+++ next.config.ts
@@ -5,17 +5,17 @@ const isGithubActions = process.env.GITHUB_ACTIONS || false;
 let assetPrefix = '';
 let basePath = '';
 
-if (isGithubActions) {
-  // Extract repository name from GITHUB_REPOSITORY (format: owner/repo)
-  const repo = "onibiocare-site";
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
-} else {
-  // For local development or other deployments, you might want to set these differently
-  // If deploying to a custom domain, leave these empty
-  assetPrefix = '';
-  basePath = '';
-}
+// if (isGithubActions) {
+//   // Extract repository name from GITHUB_REPOSITORY (format: owner/repo)
+//   const repo = "onibiocare-site";
+//   assetPrefix = `/${repo}/`;
+//   basePath = `/${repo}`;
+// } else {
+//   // For local development or other deployments, you might want to set these differently
+//   // If deploying to a custom domain, leave these empty
+//   assetPrefix = '';
+//   basePath = '';
+// }
 
 const nextConfig = {
   output: 'export',
